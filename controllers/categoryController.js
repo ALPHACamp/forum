75702123af7050a94cf8ef4e60963a1d335a3087
weiyuan <v123582@gmail.com>
2019-03-04 18:17:41 +0800

    udpate

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -15,23 +15,33 @@ let categoryController = {
     })
   },
   postCategory: (req, res) => {
-    return Category.create({
-      name: req.body.name,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    })
-     .then((category) => {
-       res.redirect('/admin/categories')
-     })
+    if(!req.body.name){
+      req.flash('error_messages', 'name didn’t exist')
+      return res.redirect('back')
+    } else {
+      return Category.create({
+        name: req.body.name,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      .then((category) => {
+        res.redirect('/admin/categories')
+      })
+    }
   },
   putCategory: (req, res) => {
-    return Category.findByPk(req.params.id)
-      .then((category) => {
-        category.updateAttributes(req.body)
+    if(!req.body.name){
+      req.flash('error_messages', 'name didn’t exist')
+      return res.redirect('back')
+    } else {
+      return Category.findByPk(req.params.id)
         .then((category) => {
-          res.redirect('/admin/categories')
+          category.updateAttributes(req.body)
+          .then((category) => {
+            res.redirect('/admin/categories')
+          })
         })
-      })
+    }
   },
   deleteCategory: (req, res) => {
     return Category.findByPk(req.params.id)
